880f5e7d3955a72b9de39db8a165d98964ccb764
Scott Moreau <oreaus@gmail.com>
2020-09-10 02:38:16 -0600

    Separate copyrights

--- wayland-logout.c
+++ wayland-logout.c
@@ -1,7 +1,8 @@
 /*
  * The MIT License (MIT)
  *
- * Copyright (c) 2020 Scott Moreau & Érico Nogueira
+ * Copyright (c) 2020 Scott Moreau
+ * Copyright (c) 2020 Érico Nogueira
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
